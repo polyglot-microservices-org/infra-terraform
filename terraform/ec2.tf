2932f6037d8c7e2413cdb28f5a35fbef1ccfd208
Yaswanth Mitta <98637209+Yaswanth-Mitta@users.noreply.github.com>
2025-09-10 12:03:55 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,13 +1,15 @@
 resource "aws_instance" "demo" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = "mittanv" 
+  key_name      = "mittanv"
 
-user_data = templatefile("${path.module}/../scripts/bootstrap.sh", {
+  # Run bootstrap script on EC2 with runner token and GH_PAT
+  user_data = templatefile("${path.module}/../scripts/bootstrap.sh", {
     runner_token = var.runner_token
+    gh_pat       = var.gh_pat
   })
 
   tags = {
-    Name = "mitta-polyglot"
+    Name = "mitta-polyglot-org"
   }
 }
