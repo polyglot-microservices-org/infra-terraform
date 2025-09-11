f72aa54cb68a456a8aa589b1521d7995106ee5d7
Yaswanth Mitta <98637209+Yaswanth-Mitta@users.noreply.github.com>
2025-09-11 05:53:58 +0000

    d3 fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+  
+  backend "s3" {
+    bucket         = "mitta-terraform-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
